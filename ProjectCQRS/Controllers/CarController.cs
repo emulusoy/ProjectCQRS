e219fa1e0a10b78780701e0115675dc139d7e9e7
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-08-25 16:16:55 +0300

    Edit

---
+++ ProjectCQRS/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCQRS.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ProjectCQRS/Program.cs
+++ ProjectCQRS/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddScoped<CQRSContext>();
 
 
 
-
 var app = builder.Build();
 
 app.UseHttpsRedirection();
